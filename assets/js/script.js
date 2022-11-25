84efec4f320a5b9a9aecbf67185ac646ca42fe2e
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-25 14:23:07 +0600

    latest appeals completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,32 @@ $(".close").click(() => {
 $(".cartContainer i").click(() => {
 	$("#sidebar-right").toggleClass("d-none");
 });
+
+// latest appeals
+$(".owl-carousel").owlCarousel({
+	loop: true,
+	margin: 30,
+	autoplay: true,
+	smartSpeed: 200,
+	nav: true,
+	navText: [
+		"<i class='fa fa-chevron-left'></i>",
+		"<i class='fa fa-chevron-right'></i>",
+	],
+	responsiveClass: true,
+	responsive: {
+		0: {
+			items: 2,
+			nav: true,
+		},
+		600: {
+			items: 2,
+			nav: true,
+		},
+		1000: {
+			items: 3,
+			nav: true,
+			loop: true,
+		},
+	},
+});
